a34328bcbb46810b6bbef4a61dbb6e92c685951e
Nidhish Nanavati <nanostudmaterial@gmail.com>
2024-09-22 18:07:02 +0530

    Added rings

--- js/script.js
+++ js/script.js
@@ -51,7 +51,8 @@ const rockClick = () =>{
     const num = Math.floor(Math.random()*3);
     const result = document.getElementById("result-output");
     const human_score = document.getElementById("human-score");
-    const computer_score = document.getElementById("computer-score"); 
+    const computer_score = document.getElementById("computer-score");
+    const ring = document.getElementsByClassName('ring-container')[0];
     console.log(num);
     if(num == 0){
         computer_spc.innerHTML = `<button class="rock" id="rock-btn"><img src="./images/icons8-fist-67.png" alt="Rock"/></button>`;
@@ -61,12 +62,20 @@ const rockClick = () =>{
         computer_spc.innerHTML = `<button class="paper" id="paper-btn"><img src="./images/icons8-hand-64.png" alt="Paper"/></button>`;
         result.innerHTML = "YOU LOST AGAINST PC<br><button id='play-again-btn'>PLAY AGAIN</button>";
 
+        // logic to move circular rings.
+        ring.style.display = "block";
+        ring.style.transform = "translate(770px,0px)";
+
         localStorage.setItem("computer-score",parseInt(localStorage.getItem("computer-score")) + 1);
         computer_score.innerHTML = localStorage.getItem("computer-score");
     }
     else{
         computer_spc.innerHTML = `<button class="scissors" id="scissors-btn"><img src="./images/icons8-finger.png" alt="Scissors"/></button>`;        
         result.innerHTML = "YOU WIN AGAINST PC<br><button id='play-again-btn'>PLAY AGAIN</button>";
+
+        ring.style.display = "block";
+        ring.style.transform = "unset";
+
         const next = document.getElementById("next-button");
         const rules = document.getElementById("rules-button");
         next.style.display = "block";
@@ -83,6 +92,8 @@ const rockClick = () =>{
         const spc_game = document.getElementsByClassName('spc-game')[0];    
         spc.style.display = "block";
         spc_game.style.display= "none";
+        ring.style.display = "none";
+        next.style.display = "none";
     }
 
     play_again.addEventListener("click",spcSelection);    
@@ -98,6 +109,7 @@ const paperClick = () =>{
     const computer_score = document.getElementById("computer-score"); 
     const num = Math.floor(Math.random()*3);
     const result = document.getElementById("result-output");
+    const ring = document.getElementsByClassName('ring-container')[0];
     console.log(num);
     if(num == 0){
         computer_spc.innerHTML = `<button class="paper" id="paper-btn"><img src="./images/icons8-hand-64.png" alt="Paper"/></button>`;
@@ -106,6 +118,10 @@ const paperClick = () =>{
     else if(num == 1){
         computer_spc.innerHTML = `<button class="rock" id="rock-btn"><img src="./images/icons8-fist-67.png" alt="Rock"/></button>`;
         result.innerHTML = "YOU WIN AGAINST PC<br><button id='play-again-btn'>PLAY AGAIN</button>";
+
+        ring.style.display = "block";
+        ring.style.transform = "translate(770px,0px)";
+
         const next = document.getElementById("next-button");
         const rules = document.getElementById("rules-button");
         next.style.display = "block";
@@ -116,6 +132,11 @@ const paperClick = () =>{
     else{
         computer_spc.innerHTML = `<button class="scissors" id="scissors-btn"><img src="./images/icons8-finger.png" alt="Scissors"/></button>`;        
         result.innerHTML = "YOU LOST AGAINST PC<br><button id='play-again-btn'>PLAY AGAIN</button>";
+
+        // logic to move circular rings.
+        ring.style.display = "block";
+        ring.style.transform = "translate(770px,0px)";
+
         if(localStorage.getItem("computer-score" == null)){
             localStorage.setItem("computer-score",1);
         }
@@ -132,6 +153,8 @@ const paperClick = () =>{
         const spc_game = document.getElementsByClassName('spc-game')[0];    
         spc.style.display = "block"
         spc_game.style.display= "none";
+        ring.style.display = "none";
+        next.style.display = "none";
     }
 
     play_again.addEventListener("click",spcSelection);     
@@ -147,6 +170,7 @@ const scissorClick = () =>{
     human_spc.innerHTML = `<button class="scissors" id="scissors-btn"><img src="./images/icons8-finger.png" alt="Scisssors"/></button>`;
     const num = Math.floor(Math.random()*3);
     const result = document.getElementById("result-output");
+    const ring = document.getElementsByClassName('ring-container')[0];    
     console.log(num);
     if(num == 0){
         computer_spc.innerHTML = `<button class="scissors" id="scissors-btn"><img src="./images/icons8-finger.png" alt="Scissor"></button>`;
@@ -155,6 +179,11 @@ const scissorClick = () =>{
     else if(num == 1){
         computer_spc.innerHTML = `<button class="paper" id="paper-btn"><img src="./images/icons8-hand-64.png" alt="Paper"/></button>`;
         result.innerHTML = "YOU WIN AGAINST PC<br><button id='play-again-btn'>PLAY AGAIN</button>";
+
+        // logic to move circular rings.
+        ring.style.display = "block";
+        ring.style.transform = "unset";
+
         const next = document.getElementById("next-button");
         const rules = document.getElementById("rules-button");
         next.style.display = "block";
@@ -166,6 +195,10 @@ const scissorClick = () =>{
     else{
         computer_spc.innerHTML = `<button class="rock" id="rock-btn"><img src="./images/icons8-fist-67.png" alt="Rock"/></button>`;     
         result.innerHTML = "YOU LOST AGAINST PC<br><button id='play-again-btn'>PLAY AGAIN</button>";
+        // logic to move circular rings.
+        ring.style.display = "block";
+        ring.style.transform = "translate(770px,0px)";
+
 
         localStorage.setItem("computer-score",parseInt(localStorage.getItem("computer-score")) + 1);
         computer_score.innerHTML = localStorage.getItem("computer-score");
@@ -178,6 +211,8 @@ const scissorClick = () =>{
         const spc_game = document.getElementsByClassName('spc-game')[0];    
         spc.style.display = "block";
         spc_game.style.display= "none";
+        ring.style.display = "none";
+        next.style.display = "none";
     }
     play_again.addEventListener("click",spcSelection);
 }
